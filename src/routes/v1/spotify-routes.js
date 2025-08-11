296d60b25b3cc95d1baf866f94d7c23b8f0d0fae
geek-neelesh <neeleshn2000@gmail.com>
2025-08-12 00:07:48 +0530

    login api

--- src/routes/v1/spotify-routes.js
+++ src/routes/v1/spotify-routes.js
@@ -2,19 +2,47 @@ const express = require('express');
 
 const router = express.Router();
 
+
 router.get('/login', function(req, res) {
 
-  const state = generateRandomString(16);
+  const state = "q1w2e3r4";
   const scope = 'user-read-private user-read-email';
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: scope,
-      redirect_uri: process.env.SPOTIFY_CALLBACK_URL,
-      state: state
-    }));
+  const params = new URLSearchParams({
+    response_type: 'code',
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    scope: scope,
+    redirect_uri: process.env.SPOTIFY_CALLBACK_URL,
+    state: state
+  });
+
+  res.redirect('https://accounts.spotify.com/authorize?' +params.toString());
 });
+
+router.get('/callback', async (req, res) => {
+  const code = req.query.code;
+  
+  try {
+    const response = await axios.post('https://accounts.spotify.com/api/token', {
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: 'YOUR_PORTFOLIO_URL/spotify/callback'
+    }, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64')}`
+      }
+    });
+    
+    // Save this refresh_token to your environment variables
+    console.log('Refresh Token:', response.data.refresh_token);
+    res.json({ refresh_token: response.data.refresh_token });
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 
 module.exports = router;
